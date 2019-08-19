1e59083e4aa97c7c2523ac6d8d78cfcc46f23e12
Napier-JP <26354173+Napier-JP@users.noreply.github.com>
2019-08-19 13:21:32 +0900

    Update template

--- _template.cpp
+++ _template.cpp
@@ -1,8 +1,12 @@
 #include <bits/stdc++.h>
 
+typedef long long ll;
 using namespace std;
+const ll divisor = 1000000007;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     return 0;
 }
