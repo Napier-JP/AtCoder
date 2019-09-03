901ac3883fe36973baef734b404b83c504551974
Napier-JP <26354173+Napier-JP@users.noreply.github.com>
2019-09-03 13:34:55 +0900

    ABC139(E AC, Python3, more straightforward)

--- abc139_e.py
+++ abc139_e.py
@@ -2,12 +2,11 @@
 from collections import deque
 
 N = int(input())
-A = [[a - 1 for a in map(int, input().split())] for _ in range(N)]
-matchAvailablePlayers = deque(range(N))  # 対戦可能な状態にある選手リスト
-daysPassedWhenPlayedMostRecentMatch = [0] * N
-opponentCandidateIdx = [-1] * N  # i番目の選手が対戦待ちをしている相手の番号が[i]に入る
+A = [deque(a - 1 for a in map(int, input().split())) for _ in range(N)]
+matchAvailablePlayers = deque(range(N))  # 対戦可能な状態にある選手リスト 最初は全選手が対戦可能状態
+daysPassedWhenPlayedMostRecentMatch = [0] * N  # 各選手が最後の試合を行ったのは何日目か？
+desiredOpponentIdx = [-1] * N  # i番目の選手が対戦を希望する相手の番号が[i]に入る
 
-# 最終日から順に遡る
 while matchAvailablePlayers:
     playerIdx = matchAvailablePlayers.popleft()
     matchesOrderForThisPlayer = A[playerIdx]  # 鏡像コピー！
@@ -15,14 +14,14 @@ while matchAvailablePlayers:
     if not matchesOrderForThisPlayer:  # この選手はもう全ての相手と対戦した
         continue
 
-    finalOpponentIdx = matchesOrderForThisPlayer.pop()  # 現時点で一番最後に対戦したい相手のindex
+    opponentIdx = matchesOrderForThisPlayer.popleft()  # 対戦したい相手のindex
 
-    if opponentCandidateIdx[finalOpponentIdx] == playerIdx:  # 対戦成立
-        daysPassedWhenPlayedMostRecentMatch[playerIdx] = daysPassedWhenPlayedMostRecentMatch[finalOpponentIdx] = max(daysPassedWhenPlayedMostRecentMatch[playerIdx], daysPassedWhenPlayedMostRecentMatch[finalOpponentIdx]) + 1  # 互いの「試合に至った日」をどちらか遅い方に合わせその翌日に書き換える
+    if desiredOpponentIdx[opponentIdx] == playerIdx:  # 対戦成立
+        daysPassedWhenPlayedMostRecentMatch[playerIdx] = daysPassedWhenPlayedMostRecentMatch[opponentIdx] = max(daysPassedWhenPlayedMostRecentMatch[playerIdx], daysPassedWhenPlayedMostRecentMatch[opponentIdx]) + 1  # 互いの「試合に至った日」をどちらか遅い方に合わせその翌日である今日に書き換える
         matchAvailablePlayers.append(playerIdx)  # 試合終了、対戦可能リストに加える
-        matchAvailablePlayers.append(finalOpponentIdx)
-    else:  # 対戦できない、このプレイヤーは対戦待ちをする
-        opponentCandidateIdx[playerIdx] = finalOpponentIdx
+        matchAvailablePlayers.append(opponentIdx)
+    else:  # 相手が自分との対戦を望んでいないので対戦できない、このプレイヤーは対戦待ちをする
+        desiredOpponentIdx[playerIdx] = opponentIdx
 
 if any(A):  # リストのリストなので、if A:だと空リストが中に存在しているだけでもTrueになってしまう
     print(-1)
